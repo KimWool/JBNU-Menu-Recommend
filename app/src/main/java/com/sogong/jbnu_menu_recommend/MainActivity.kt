fc3f12e3419dae30e7fade46c8a32692daa39742
KimWool <84843984+KimWool@users.noreply.github.com>
2022-11-28 01:41:02 +0900

    bottom menu 구현

--- app/src/main/java/com/sogong/jbnu_menu_recommend/MainActivity.kt
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/MainActivity.kt
@@ -2,11 +2,43 @@ package com.sogong.jbnu_menu_recommend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_JBNUMenuRecommend)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavi = findViewById<BottomNavigationView>(R.id.bottomNavi)
+
+        bottomNavi.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.aciton_home -> {
+                        val homeFragment = Home()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, homeFragment).commit()
+                    }
+                    R.id.aciton_location -> {
+                        val locationFragment = Location()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, locationFragment).commit()
+                    }
+                    R.id.aciton_wallet -> {
+                        val walletFragment = Wallet()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, walletFragment).commit()
+                    }
+                    R.id.aciton_person -> {
+                        val personFragment = Person()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, personFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.aciton_home
+        }
     }
 }
