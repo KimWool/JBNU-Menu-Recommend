4ebab38c581e8ec98d451719941516d8c80e3a4e
KimWool <84843984+KimWool@users.noreply.github.com>
2022-12-01 00:06:33 +0900

    RegisterActivity 추가

---
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.sogong.jbnu_menu_recommend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
