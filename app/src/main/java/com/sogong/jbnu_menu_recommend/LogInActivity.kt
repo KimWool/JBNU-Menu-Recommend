8726d7aad8358612264b2b750157b0587818a5af
KimWool <84843984+KimWool@users.noreply.github.com>
2022-12-02 23:47:15 +0900

    splash 화면 수정

--- app/src/main/java/com/sogong/jbnu_menu_recommend/LogInActivity.kt
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/LogInActivity.kt
@@ -1,11 +1,23 @@
 package com.sogong.jbnu_menu_recommend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sogong.jbnu_menu_recommend.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //회원가입 버튼 이벤트
+        binding.register.setOnClickListener {
+            val intent: Intent = Intent(this@LogInActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/sogong/jbnu_menu_recommend/RegisterActivity.kt
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/RegisterActivity.kt
@@ -1,11 +1,47 @@
 package com.sogong.jbnu_menu_recommend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.sogong.jbnu_menu_recommend.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = Firebase.auth
+
+        binding.registerButton.setOnClickListener {
+            val email = binding.registerId.text.toString().trim()
+            val password = binding.registerPassword.text.toString().trim()
+            //비밀번호 재확인, 이름 입력 부분 추가
+
+            signUp(email, password)
+        }
+    }
+
+    //회원가입 기능 구현
+    private fun signUp(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "환영합니다!", Toast.LENGTH_SHORT).show()
+                    val intent: Intent = Intent(this@RegisterActivity, LogInActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "회원가입에 실패하였습니다.\n다시 한번 확인해 주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
