6bc0a79ee5691dbe869190f6b51ff73023fa147a
KimWool <84843984+KimWool@users.noreply.github.com>
2022-12-03 03:22:05 +0900

    Register 화면 구현 (소속 단과대학 선택 spinner)

--- app/src/main/java/com/sogong/jbnu_menu_recommend/RegisterActivity.kt
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/RegisterActivity.kt
@@ -1,9 +1,14 @@
 package com.sogong.jbnu_menu_recommend
 
+//noinspection SuspiciousImport
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -11,9 +16,9 @@ import com.sogong.jbnu_menu_recommend.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityRegisterBinding
 
-    lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,17 @@ class RegisterActivity : AppCompatActivity() {
             //비밀번호 재확인, 이름 입력 부분 추가
 
             signUp(email, password)
+        }
+
+        val data = listOf("- 선택하세요 -", "간호대학", "공과대학", "글로벌융합대학", "농업생명과학대학", "사범대학", "사회과학대학", "상과대학", "생활과학대학", "수의과대학", "스마트팜학과", "약학대학", "예술대학", "의과대학", "인문대학", "자연과학대학", "치과대학", "환경생명자원대학")
+
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, data)
+
+        binding.departmentSpinner.adapter = adapter
+
+        binding.departmentSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?){}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
         }
     }
 
