77e255c83b7bcb0ccb7881f63f9089e5f961bbbf
kimsk79 <kdasmekl@gmail.com>
2022-12-05 02:52:26 +0900

    밥값내기_1/N 계산하기 구현 중

---
+++ app/src/main/java/com/sogong/jbnu_menu_recommend/SFragment.kt
@@ -0,0 +1,69 @@
+package com.sogong.jbnu_menu_recommend
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sogong.jbnu_menu_recommend.databinding.FragmentSBinding
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+//fragment 화면 전환: walletFragment -> SFragment (사다리 타기 화면)
+class SFragment : Fragment() {
+    lateinit var mainActivity: MainActivity
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    //DFragment.kt의 버튼을 클릭했을 때 WalletFragment.kt로 돌아감
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSBinding.inflate(inflater, container, false)
+        binding.Back.setOnClickListener{mainActivity.goBack()}
+        return binding.root
+        // Inflate the layout for this fragment
+        // return inflater.inflate(R.layout.fragment_d, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
